1d3b4b548c0ca77897123b239b7e5256a211199a
elliottjf <elliottj@pobox.com>
2016-03-19 18:36:02 -0700

    fix admin profile view

--- app/admin/profile.rb
+++ app/admin/profile.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Profile do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 
-permit_params :name, :first_name, :last_name, :email, :phone
+  permit_params :name, :first_name, :last_name, :email, :phone, :data_json
 
 #
 # or
@@ -13,6 +13,27 @@ permit_params :name, :first_name, :last_name, :email, :phone
 #   permitted << :other if resource.something?
 #   permitted
 # end
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    actions
+  end
+
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+      f.input :phone
+      f.input :data_json, as: :text
+    end
+    f.actions
+  end
 
 
 end
