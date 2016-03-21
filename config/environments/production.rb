acffa55f72829c13a1c3bb6701f051972d5512b1
elliottjf <elliottj@pobox.com>
2016-03-21 00:38:37 -0700

    rip out prod devise ssl config for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Devise::SessionsController.ssl_required :new, :create
+  # Devise::SessionsController.ssl_required :new, :create
 
 end
