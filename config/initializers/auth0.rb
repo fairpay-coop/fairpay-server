0187c80613a2a319c1f974e55ed49425a1d89edf
elliottjf <elliottj@pobox.com>
2016-05-01 14:29:37 -0700

    auth0 config fix

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,13 +1,15 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # todo: support multiple user realms and auth0 provider configs
   auth0_config = MerchantConfig.where(kind: 'auth0').first
-  provider(
-    :auth0,
-    auth0_config.get_data_field(:client_id),
-    auth0_config.get_data_field(:client_secret),
-    auth0_config.get_data_field(:domain),
-    callback_path: "/auth/auth0/callback"
-  )
+  if auth0_config
+    provider(
+      :auth0,
+      auth0_config.get_data_field(:client_id),
+      auth0_config.get_data_field(:client_secret),
+      auth0_config.get_data_field(:domain),
+      callback_path: "/auth/auth0/callback"
+    )
+  end
 
   # provider(
   #   :auth0,
