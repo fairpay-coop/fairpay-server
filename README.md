01071e7107f2aa2918221a69e1db3219a4d80f27
elliottjf <elliottj@pobox.com>
2016-06-14 22:04:47 -0700

    fix auth0 initializer which depended on db and causing db creation rake task to fail

--- README.md
+++ README.md
@@ -11,6 +11,9 @@
 $ git clone git@github.com:fairpay-coop/fairpay-server.git
 $ cd fairpay-server
 $ cp config/database-example.yml config/database.yml
+# edit database.yml as appropriate
+$ cp dotenv-example .env
+# edit .env appropriate (may wish to ask for example file with credentials from another dev)
 $ bundle exec rake db:create db:migrate
 $ bundle exec rails s
 ```
--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,21 +1,23 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # todo: support multiple user realms and auth0 provider configs
-  auth0_config = MerchantConfig.where(kind: 'auth0').first
-  if auth0_config
-    provider(
-      :auth0,
-      auth0_config.get_data_field(:client_id),
-      auth0_config.get_data_field(:client_secret),
-      auth0_config.get_data_field(:domain),
-      callback_path: "/auth/auth0/callback"
-    )
-  end
+  # beware, this code broke the ability to run the 'rake db:' commands since it tries to
+  # executed before running the rake tasks
+  # auth0_config = MerchantConfig.where(kind: 'auth0').first
+  # if auth0_config
+  #   provider(
+  #     :auth0,
+  #     auth0_config.get_data_field(:client_id),
+  #     auth0_config.get_data_field(:client_secret),
+  #     auth0_config.get_data_field(:domain),
+  #     callback_path: "/auth/auth0/callback"
+  #   )
+  # end
 
-  # provider(
-  #   :auth0,
-  #   ENV["AUTH0_CLIENT_ID"],
-  #   ENV["AUTH0_CLIENT_SECRET"],
-  #   ENV["AUTH0_DOMAIN"],
-  #   callback_path: "/auth/auth0/callback"
-  # )
+  provider(
+    :auth0,
+    ENV["AUTH0_CLIENT_ID"],
+    ENV["AUTH0_CLIENT_SECRET"],
+    ENV["AUTH0_DOMAIN"],
+    callback_path: "/auth/auth0/callback"
+  )
 end
