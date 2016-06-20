8638797d502587dbf424a03bd63244eaed07398b
elliottjf <elliottj@pobox.com>
2016-06-20 12:37:55 -0700

    update deploy file for abuntoo branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,13 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.5.0'
 
 set :rvm_ruby_version, '2.3.0'
 set :application, 'fairpay-server'
 set :repo_url, 'git@github.com:fairpay-coop/fairpay-server.git'
 
 # Default branch is :master
-set :branch, 'develop'
+#set :branch, 'develop'
+set :branch, 'abuntoo'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
